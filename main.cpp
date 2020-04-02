90fb5fbf4dd5f7bae73985b0e539705dcc0bed2b
aleksandr <mac@MacBook-Air-Mas.local>
2020-04-02 18:22:55 +0300

    selection method Липницкий Александр

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <algorithm>
 
 #define PopSize 10
 #define ChromoDim 10
@@ -53,6 +54,39 @@ vector<vector<bool>> Selection(vector<vector<bool>> Generation) {
     // TODO DIMA
 }
 
+vector<bool> SelectionV2(vector<vector<bool>> Generation) {
+    // Метод выбора
+    // Массив фитнесс функций
+    vector<int> chr_fitness;
+    for (int i(0); i < Generation.size(); ++i) {
+        chr_fitness.push_back(FitnessFunction(Generation[i]));
+    }
+    // Сортируем массив
+    sort(chr_fitness.rbegin(), chr_fitness.rend());
+    // Массив индексов хромосом в популяции, отсортированный по убыванию фитнесс функции
+    vector<int> indexes;
+    for (int i(0); i < chr_fitness.size(); ++i) {
+        for (int j(0); j < Generation.size(); ++j) {
+            if (chr_fitness[i] == FitnessFunction(Generation[j])) {
+                indexes.push_back(j)
+            }
+        }
+    }
+    // Выбираем лучшую родительскую хромосому для следующей популяции
+    vector<vector<bool>> intermediate_population;
+    srand(time(0));
+    int groups_count = Generation.size() / 4
+
+    int number = rand() % 100;
+    if (number < 50)
+        return Generation[indexes[rand() % groups_count]];
+    else if ((number >= 50) && (number < 80))
+        return Generation[indexes[(rand() % groups_count) + groups_count]];
+    else if ((number >= 80) && (number < 95))
+        return Generation[indexes[(rand() % groups_count) + 2 * groups_count]];
+    else if ((number >= 95) && (number < 100))
+        return Generation[indexes[(rand() % groups_count) + 3 * groups_count]];
+}
 
 vector<bool> Crossingover(vector<vector<bool>> Generation) {
     // TODO DIMA
